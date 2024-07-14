25b4f59b42b14c66bf553a2a12082540cb979282
ClauCelis <ps.celis@gmail.com>
2024-07-14 18:32:54 -0400

    Cambios varios Tooltip, Favicon, Botón carrusel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip();
+    
+});
+
+
 $( "#enviarCorreo" ).on( "click", function() {
     alert( "Te estaremos contactando pronto. ¡Muchas gracias!" );
+    setTimeout(function() {
+        $('#enviarCorreo').tooltip('hide');
+    }, 500);
 });
+
